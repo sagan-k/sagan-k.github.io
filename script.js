5c7d5f244008abb2a7a34a442a0de81aec0eb6c3
sagan-k <sagankhurme@gmail.com>
2026-04-13 16:03:40 -0400

    Added pages, configurated CSS.

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ function changeColor() {
     const colors = ["rgb(255, 0, 0)", "rgb(255, 115, 0)", "rgb(238, 255, 0)", "rgb(67, 255, 20)", "rgb(22, 56, 248)", "rgb(140, 0, 255)"];
     let randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.getElementById("title").style.color=randomColor;
+
 }
 
 let themeNum = 0;
 function changeTheme() {
-    const themes = ["", "theme-purple","theme-jade"];
+    const themes = ["theme-purple","theme-jade", "theme-ocean"];
     themeNum += 1;
     if (themeNum >= themes.length) {
         themeNum = 0;
@@ -19,6 +20,12 @@ function changeTheme() {
 function defaultTheme() {
     document.body.className = "";
     document.getElementById("title").style.color="";
+    document.getElementById("title").classList.remove("rainbowAnim");
     document.getElementById("themeDeclare").innerHTML = "Theme:" + " Default";
     
+}
+
+
+function rainbow() {
+    document.getElementById("body").classList.toggle("rainbowAnim");
 }
